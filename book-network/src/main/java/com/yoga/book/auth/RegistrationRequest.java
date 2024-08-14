28c7a6f1879e8eb5011add191f54c06657bd00f9
yogante <yo_yo_ga@yahoo.co.jp>
2024-08-15 00:38:07 +0900

    Remove submodule and add book-network as a regular directory

---
+++ book-network/src/main/java/com/yoga/book/auth/RegistrationRequest.java
@@ -0,0 +1,34 @@
+package com.yoga.book.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotEmpty(message = "Firstname is mandatory.")
+    @NotBlank(message = "Firstname is mandatory.")
+    private String firstname;
+
+    @NotEmpty(message = "Lastname is mandatory.")
+    @NotBlank(message = "Lastname is mandatory.")
+    private String lastname;
+
+    @Email(message = "Email is not formatted.")
+    @NotEmpty(message = "Email is mandatory.")
+    @NotBlank(message = "Email is mandatory.")
+    private String email;
+
+    @NotEmpty(message = "Password is mandatory.")
+    @NotBlank(message = "Password is mandatory.")
+    @Size(min = 8, message = "pass should be 8 characters long minimum.")
+    private String password;
+}
---
+++ book-network/src/main/java/com/yoga/book/security/JwtService.java
@@ -0,0 +1,92 @@
+package com.yoga.book.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    public String extraUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaim(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaim(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String generateToken(UserDetails userDetails){
+    return generateToken(new HashMap<>(), userDetails);
+
+}
+
+    public String generateToken(
+            HashMap<String, Object> claims,
+            UserDetails userDetails) {
+    return buildToken(claims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(
+            HashMap<String, Object> extraClaims,
+            UserDetails userDetails,
+            long jwtExpiration) {
+        var authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claim("authorities", authorities)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extraUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extraExpiration(token).before(new Date());
+    }
+
+    private Date extraExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+}
